ce1e436e2ab624c0f61c8cbd6f8b4af3c6e6a9eb
Mark <nirvanafoo86@hotmail.com>
2020-08-17 16:47:03 +0100

    created form and sign in auth

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,64 @@
 <script>
+  let userObject = null;
   const userbase = window.userbase;
-  userbase.init({ appId: "5c55b6f7-bd53-4707-877d-0e4a87d274c6" });
+
+  let authPromise = userbase
+    .init({ appId: "5c55b6f7-bd53-4707-877d-0e4a87d274c6" })
+    .then(({ user }) => (userObject = user));
   let username, password;
 
-  const signIn = () => userbase.signIn({ username, password });
-  const signUp = () => userbase.signUp({ username, password });
+  const signIn = () =>
+    (authPromise = userbase
+      .signIn({ username, password })
+      .then(user => (userObject = user)));
+
+  const signUp = () =>
+    (authPromise = userbase
+      .signUp({ username, password })
+      .then(user => (userObject = user)));
+
+  const signOut = () => {
+    authPromise = userbase.signOut().then((userObject = null));
+  };
 </script>
 
-<form>
-  <label for="username">Username</label>
-  <input id="username" type="text" bind:value={username} />
-  <label for="password">Password</label>
-  <input id="password" type="password" bind:value={password} />
-  <button on:click={signUp} type="button">Sign Up</button>
-  <button on:click={signIn} type="button">Sign In</button>
-</form>
+<style>
+  .form-container {
+    display: flex;
+    justify-content: center;
+  }
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 50%;
+  }
+  .sign-out {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+  }
+</style>
+
+{#await authPromise}
+  <h3>Loading...</h3>
+{:then _}
+  {#if !userObject}
+    <div class="form-container">
+      <form>
+        <label for="username">Username</label>
+        <input id="username" type="text" bind:value={username} />
+        <label for="password">Password</label>
+        <input id="password" type="password" bind:value={password} />
+        <button on:click={signUp} type="button">Sign Up</button>
+        <button on:click={signIn} type="button">Sign In</button>
+      </form>
+    </div>
+  {:else}
+    <h1>Hi, {username}!</h1>
+    <button class="sign-out" on:click={signOut}>Sign Out</button>
+  {/if}
+
+{:catch error}
+  There was a problem {error}
+{/await}
