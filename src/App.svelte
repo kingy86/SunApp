574fbba93ec2aa26f19fce2f827599a82588d556
Mark <nirvanafoo86@hotmail.com>
2020-08-17 12:12:51 +0100

    adding UserBase credentials

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,6 @@
 <script>
-	export let name;
+  const userbase = window.userbase;
+  const appId = "5c55b6f7-bd53-4707-877d-0e4a87d274c6";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<h1>Hello Sun App!</h1>
