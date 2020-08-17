2b9cb815e12f5642316cc6a23514f7d081ee2dda
Mark <nirvanafoo86@hotmail.com>
2020-08-17 12:30:51 +0100

    setup form and updated userbase cred

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,17 @@
 <script>
   const userbase = window.userbase;
-  const appId = "5c55b6f7-bd53-4707-877d-0e4a87d274c6";
+  userbase.init({ appId: "5c55b6f7-bd53-4707-877d-0e4a87d274c6" });
+  let username, password;
+
+  const signIn = () => userbase.signIn({ username, password });
+  const signUp = () => userbase.signUp({ username, password });
 </script>
 
-<h1>Hello Sun App!</h1>
+<form>
+  <label for="username">Username</label>
+  <input id="username" type="text" bind:value={username} />
+  <label for="password">Password</label>
+  <input id="password" type="password" bind:value={password} />
+  <button on:click={signUp} type="button">Sign Up</button>
+  <button on:click={signIn} type="button">Sign In</button>
+</form>
